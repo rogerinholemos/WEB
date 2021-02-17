a01b6196776344f427b42eca50fa878b5b32e0d8
rogerinholemos <52638627+rogerinholemos@users.noreply.github.com>
2021-02-17 16:19:02 -0300

    Create main.js

---
+++ pratica3/scripts/main.js
@@ -0,0 +1,34 @@
+let minhaImagem = document.querySelector('img');
+
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+    if(meuSrc === 'images/firefox-icon.png') {
+      minhaImagem.setAttribute ('src','images/firefox2.png');
+    } else {
+      minhaImagem.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+let meuBotao = document.querySelector('button');
+let meuCabecalho = document.querySelector('h1');
+
+function defineNomeUsuario() {
+    let meuNome = prompt('Por favor, digite seu nome.');
+    if(!meuNome || meuNome === null) {
+      defineNomeUsuario();
+    } else {
+      localStorage.setItem('nome', meuNome);
+      meuCabecalho.innerHTML = 'Mozilla is cool, ' + meuNome;
+    }
+}
+
+if(!localStorage.getItem('nome')) {
+    defineNomeUsuario();
+} else {
+    let nomeGuardado = localStorage.getItem('nome');
+    meuCabecalho.textContent = 'Mozilla is cool, ' + nomeGuardado;
+}
+
+meuBotao.onclick = function() { 
+    defineNomeUsuario();
+}
